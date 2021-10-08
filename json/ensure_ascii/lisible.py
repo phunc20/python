3debfe450c3570fc0c1455f21f5a8e234dcb3c87
phunc20 <wucf20@gmail.com>
2021-10-08 14:24:12 +0700

    bug for path as str not as Path object

--- json/ensure_ascii/lisible.py
+++ json/ensure_ascii/lisible.py
@@ -1,11 +1,12 @@
 import argparse
 import json
 from pathlib import Path
+import os
 
 def save_lisible(filename):
     with open(filename, "r") as f:
         D = json.load(f)
-    stem = filename.stem if isinstance(filename, Path) else filename.split(".")[0]
+    stem = filename.stem if isinstance(filename, Path) else os.path.splitext(os.path.basename(filename))[0]
     with open(f"{stem}_lisible.json", "w") as f:
         json.dump(D, f, ensure_ascii=False, indent=2)
 
