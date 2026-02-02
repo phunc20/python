9d5057aa264e4cc866e8f8a85b4d4627138bf904
phunc20 <wucf20@gmail.com>
2026-02-02 16:26:07 +0800

    stop here on developping dash-man/callback/MATCH/examples/todo_list_app.py

--- dash-man/callback/MATCH/examples/todo_list_app.py
+++ dash-man/callback/MATCH/examples/todo_list_app.py
@@ -1,113 +1,89 @@
 from dash import (
-    Dash, dcc, html, Input, Output, State,
-    MATCH, ALL, Patch, callback, no_update,
-    callback_context,
+    ALL,
+    MATCH,
+    Dash,
+    Input,
+    Output,
+    Patch,
+    State,
+    callback,
+    #no_update,
+    #callback_context,
+    dcc,
+    html,
 )
-from dash.exceptions import PreventUpdate
+
+#from dash.exceptions import PreventUpdate
 
 # TODO:
-# 1. Checklist label instead of output-str
-# 2. More the alignment prettier
-# 3. Add a check-all-btn
+# 1. [x] Checklist label instead of output-str
+# 2. CSS (style)
+#    - [ ] Make the alignment prettier
+#    - [ ] The item/checkbox label not aligned with checkbox when str is too long. Bad
+# 3. [x] Add a toggle-all-btn
+# 4. refactorization
+#    - [ ] naming of functions, variables
+#    - [x] import order
+# 5. [ ] When the list is so long that a scrollbar appears, auto-focus to last item
 
 app = Dash()
 
 app.layout = html.Div(
     [
-        dcc.Checklist(
-            options=[{"label": "anything", "value": "done"}],
-            #id={"index": button_clicked, "type": "done"},
-            #style={"display": "inline", "text-align": "left"},
-            #labelStyle={"display": "inline"},
-            style={"text-decoration": "line-through"}
-        ),
-        html.Div(
+        html.H1(
             "Dash To-Do list",
             style={
                 "text-align": "center",
             },
         ),
         html.Div(
-            id="todo-list-div",
+            id="control-div",
             children=[
-                html.Div(
-                    id="list-controller-div",
-                    children=[
-                        dcc.Input(id="new-item-input", n_submit=0),
-                        html.Button("Add", id="add-btn"),
-                        html.Button(
-                            "Clear Done",
-                            id="clear-done-btn",
-                        ),
-                    ],
-                    style={
-                        #"display": "inline",
-                        "display": "block",
-                        #"text-align": "center",
-                    },
+                dcc.Input(id="new-item-input", n_submit=0),
+                html.Button("Add", id="add-btn"),
+                html.Button(
+                     "Clear Done",
+                     id="clear-done-btn",
                 ),
-                html.Div(
-                    id="list-container-div",
-                    style={
-                        #"display": "flex",
-                        #"justify-content": "center",
-                        #"align-items": "center",
-                        #"display": "grid",
-                        #"place-items": "center",
-
-                        #"display": "inline-block",
-                        #"display": "inline",
-                        "display": "block",
-                        "text-align": "left",
-                        #"display": "flex",
-                        #"justify-content": "center",
-                        #"width": "100%",
-                    },
-                ),
-                html.Div(
-                    id="totals-div",
-                    style={
-                        #"display": "inline-block",
-                        "display": "block",
-                        #"display": "inline",
-                        #"text-align": "left",
-                        #"text-align": "center",
-                    },
+                html.Button(
+                     "Toggle All",
+                     id="toggle-all-btn",
                 ),
             ],
             style={
-                #"align-items": "center",
-
-                #"display": "flex",
-                #"justify-content": "center",
-                #"width": "100%",
-                #"text-align": "center",
+                "display": "flex",
+                "justify-content": "center",
+                "gap": "5px",
+                "margin-bottom": "10px"
             },
         ),
-        #dcc.Input(id="new-item-input", n_submit=0),
-        #html.Button("Add", id="add-btn"),
-        #html.Button("Clear Done", id="clear-done-btn"),
-        #html.Div(id="list-container-div"),
-        #html.Div(id="totals-div"),
-    ]
+        html.Div(
+            id="list-container-div",
+            style={
+                "margin": "0 auto",      # Centers the box horizontally
+                "width": "300px",        # FIXED WIDTH prevents shifting when items change
+                "display": "flex",
+                "max-height": "250px",
+                "overflow-y": "auto",
+                "flex-direction": "column",
+                "align-items": "flex-start", # Keeps checkboxes flush left inside the 300px box
+                #"gap": "5px"             # Tight vertical spacing
+            },
+         ),
+        html.Div(
+            id="totals-div",
+            style={
+                "text-align": "center",
+                "margin-top": "20px",
+                #"display": "inline-block",
+                #"display": "block",
+                #"display": "inline",
+                #"text-align": "left",
+            },
+        ),
+    ],
+    style={"width": "100%"},
 )
-
-
-@callback(
-    Output("add-btn", "n_clicks"),
-    Input("new-item-input", "n_submit"),
-    State("add-btn", "n_clicks"),
-    prevent_initial_call=True,
-)
-def enable_enter_keypress(n_submit, n_clicks):
-    """
-    When the user type an item in the input text box and press Enter,
-    this should have the same effet as clicking the add button.
-    """
-    if n_clicks is None:
-        return 1
-    else:
-        return n_clicks + 1
 
 
 @callback(
@@ -125,7 +101,6 @@ def add_item(n_clicks: int, item: str):
     one todo item, i.e. we return an empty string `""`.
     """
     def new_checklist_item():
-        print(f'{item = }')
         return html.Div(
             children=dcc.Checklist(
                 options=[{"label": item if item else "", "value": "done"}],
@@ -135,7 +110,8 @@ def add_item(n_clicks: int, item: str):
                 #labelStyle={},
             ),
             style={
-                "display": "flex",
+                #"display": "flex",
+                "display": "inline",
                 "align-items": "center",
                 "padding": "2px 0" # Vertical tightness
             }
@@ -148,105 +124,17 @@ def add_item(n_clicks: int, item: str):
     return patched_list, ""
 
 
-#    #def new_checklist_item():
-#    #    return html.Div(
-#    #        [
-#    #            dcc.Checklist(
-#    #                options=[{"label": "", "value": "done"}],
-#    #                id={"index": button_clicked, "type": "done"},
-#    #                style={"display": "inline", "text-align": "left"},
-#    #                labelStyle={"display": "inline"},
-#    #            ),
-#    #            html.Div(
-#    #                [value],
-#    #                id={"index": button_clicked, "type": "output-str"},
-#    #                style={"display": "inline", "margin": "10px"},
-#    #            ),
-#    #        ]
-#    #    )
-#
-#    def new_checklist_item():
-#        #return html.Div(
-#        #    [
-#        #        dcc.Checklist(
-#        #            options=[{"label": "", "value": "done"}],
-#        #            id={"index": button_clicked, "type": "done"},
-#        #            style={"margin-right": "10px"},
-#        #        ),
-#        #        html.Div(
-#        #            [value],
-#        #            id={"index": button_clicked, "type": "output-str"},
-#        #            style={"flex": "1"}, # Allows text to take up remaining space
-#        #        ),
-#        #    ],
-#        #    style={
-#        #        "display": "flex",          # Turn on Flexbox
-#        #        "align-items": "center",    # Vertically aligns checkbox with text
-#        #        "justify-content": "center",# Centers the whole row horizontally
-#        #        "max-width": "500px",       # Prevents the list from getting too wide
-#        #        "margin": "0 auto",         # Centers the container itself on the page
-#        #        "padding": "10px"
-#        #    }
-#        #)
-#
-#        #return html.Div(
-#        #    [
-#        #        dcc.Checklist(
-#        #            options=[{"label": "", "value": "done"}],
-#        #            id={"index": button_clicked, "type": "done"},
-#        #            # inline-block keeps it from forcing a new line
-#        #            style={"display": "inline-block", "vertical-align": "middle"},
-#        #            inputStyle={"margin": "0"} # Removes default checkbox margin
-#        #        ),
-#        #        html.Div(
-#        #            [value],
-#        #            id={"index": button_clicked, "type": "output-str"},
-#        #            style={"display": "inline-block", "vertical-align": "middle"}
-#        #        ),
-#        #    ],
-#        #    style={
-#        #        "display": "flex",
-#        #        "align-items": "center",
-#        #        "justify-content": "center", # Centers the whole group
-#        #        "gap": "8px",                # Controlled pixel-perfect horizontal gap
-#        #        "padding": "2px 0",          # Tight vertical spacing between rows
-#        #        "line-height": "1",          # Collapses vertical text height
-#        #    }
-#        #)
-#        return html.Div(
-#            [
-#                dcc.Checklist(
-#                    options=[{"label": "", "value": "done"}],
-#                    id={"index": button_clicked, "type": "done"},
-#                    style={"display": "inline-block", "vertical-align": "middle"},
-#                    inputStyle={"margin": "0"}
-#                ),
-#                html.Div(
-#                    value,
-#                    id={"index": button_clicked, "type": "output-str"},
-#                    style={"display": "inline-block", "vertical-align": "middle", "margin-left": "8px"},
-#                ),
-#            ],
-#            style={
-#                "display": "flex",
-#                "align-items": "center",
-#                "padding": "2px 0" # Vertical tightness
-#            }
-#        )
-
-
 @callback(
     Output({"index": MATCH, "type": "done"}, "style"),
     Input({"index": MATCH, "type": "done"}, "value"),
     prevent_initial_call=True,
 )
-def toggle_checkbox(value):
+def toggle_checkbox_style_change(value):
     """
     The only `value` I have seen are
     - `[]`
     - `["done"]`
     """
-    print(f'(checkbox_check_and_decheck) {value = }')
     #if isinstance(value, list):
     style = Patch()
     if len(value) == 0:
@@ -254,12 +142,6 @@ def toggle_checkbox(value):
         style["text-decoration"] = None
         style["color"] = None
     elif value[0] == "done":
-        #style = {
-        #    "display": "inline",
-        #    #"margin": "10px",
-        #    "textDecoration": "line-through",
-        #    "color": "#888",
-        #}
         style["text-decoration"] = "line-through"
         style["color"] = "gray"
     return style
@@ -295,6 +177,42 @@ def show_totals(done):
     if count_all:
         result += f" - {int(100 * count_done / count_all)}%"
     return result
+
+
+# The following callbacks are COMPLEMENTARY and
+# may be disabled without affecting the basic functionality
+
+@callback(
+    Output("add-btn", "n_clicks"),
+    Input("new-item-input", "n_submit"),
+    State("add-btn", "n_clicks"),
+    prevent_initial_call=True,
+)
+def enable_enter_keypress(n_submit, n_clicks):
+    """
+    When the user type an item in the input text box and press Enter,
+    this should have the same effet as clicking the add button.
+    """
+    if n_clicks is None:
+        return 1
+    else:
+        return n_clicks + 1
+
+
+@callback(
+    Output({"index": ALL, "type": "done"}, "value"),
+    Input("toggle-all-btn", "n_clicks"),
+    State({"index": ALL, "type": "done"}, "value"),
+    prevent_initial_call=True,
+)
+def toggle_all_items(n_clicks, current_values):
+    #print(f'{current_values = }')
+    # Using any/all provides diff behaviors
+    if all(current_values):
+        #return [None] * len(current_values) # Wrong, but hard to explain
+        return [[]] * len(current_values)
+    else:
+        return [["done"]] * len(current_values)
 
 
 if __name__ == "__main__":
